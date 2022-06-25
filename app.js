293507c88dd7a12a30bc5c2fbb27e550468f438d
Naoufal Aziane <naoufal.aziane@pm.me>
2022-06-25 14:14:16 +0100

    Create the UI variables and select them from the dom

---
+++ app.js
@@ -0,0 +1,9 @@
+// Create the UI variables and select the dom
+
+const roundCounter = document.querySelector('.round-counter');
+const score = document.querySelector('.score');
+const roundWinner = document.querySelector('.round-winner');
+const weapons = document.querySelectorAll('.weapons > *');
+const winnerText = document.querySelector('.winner h1');
+const playAgainBtn = document.querySelector('.winner button');
+
