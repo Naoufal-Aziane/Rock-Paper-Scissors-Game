6f9007e7b4a375c1b8f1788b2297d593ce296471
Naoufal Aziane <naoufal.aziane@pm.me>
2022-06-25 14:41:13 +0100

    Create the game() function to start the game

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Create the UI variables and select the dom
+// Create the UI variables and select them from the dom
 
 const roundCounter = document.querySelector('.round-counter');
 const score = document.querySelector('.score');
@@ -6,4 +6,15 @@ const roundWinner = document.querySelector('.round-winner');
 const weapons = document.querySelectorAll('.weapons > *');
 const winnerText = document.querySelector('.winner h1');
 const playAgainBtn = document.querySelector('.winner button');
+
+// The game functionality
+let items = ['rock','paper','scissors'];
+
+function game() {
+    let userChoice = items.indexOf(this.className);
+}
+
+weapons.forEach(choice => {
+    choice.addEventListener('click',game)
+});
 
