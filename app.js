679938b7f497211b7ca215e839041bf0037bb9ae
Naoufal Aziane <naoufal.aziane@pm.me>
2022-06-25 14:50:22 +0100

    Create getComputerChoice() and getPlayerChoice() functions

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 // Create the UI variables and select them from the dom
 
-const roundCounter = document.querySelector('.round-counter');
+const roundNumber = document.querySelector('.round-number');
 const score = document.querySelector('.score');
 const roundWinner = document.querySelector('.round-winner');
 const weapons = document.querySelectorAll('.weapons > *');
 const winnerText = document.querySelector('.winner h1');
 const playAgainBtn = document.querySelector('.winner button');
 
-// The game functionality
+// The game function
 let items = ['rock','paper','scissors'];
 
 function game() {
-    let userChoice = items.indexOf(this.className);
+    
 }
 
 weapons.forEach(choice => {
     choice.addEventListener('click',game)
 });
 
+function getComputerChoice () {
+    return Math.floor(Math.random()*3);  
+}
+
+function getPlayerChoice (weapon) {
+    return items.indexOf(weapon.className);
+}
